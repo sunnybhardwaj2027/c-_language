ccd503e0ba01fdc8cc2035fe09354bb23bd4f96c
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-02-20 22:08:45 +0530

    basic question based on cpp language

---
+++ basic_exercise.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+// Q2--Write a Cpp program to enter two numbers and find their sum
+/*
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << "sum: " << a+b << endl;
+}
+*/
+
+// Q3--Write a Cpp program to enter two numbers and perform all arithmetic operations
+/*
+int main()
+{
+    int num1, num2, sum, difference, product, modulus, quotient;
+
+    cout << "num1: ";
+    cin >> num1;
+    
+    cout << "num2: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    modulus = num1 % num2;
+    quotient = num1 / num2;
+
+    cout << "sum: " << sum << endl << "difference: " << difference << endl << "product: " << product << endl << "modulus: " << modulus << endl << "quotient: " << quotient << endl;
+}
+*/
+
+// Q4--Write a Cpp program to enter length and breadth of a rectangle and find its perimeter
+/*
+int main()
+{
+    float length, breadth, perimeter;
+    cout << "length: ";
+    cin >> length;
+
+    cout << "breadth: ";
+    cin >> breadth;
+
+    perimeter = 2 * (length + breadth);
+
+    cout << "perimeter: " << perimeter << endl;
+}
+*/
+
+// Q5--Write a Cpp program to enter length and breadth of a rectangle and find its area
+/*
+int main()
+{
+    float length, breadth, area;
+    cout << "length: ";
+    cin >> length;
+
+    cout << "breadth: ";
+    cin >> breadth;
+
+    area = length * breadth;
+
+    cout << "area: " << area << endl;
+}
+*/
+
+// Q6--Write a Cpp program to enter radius of a circle and find its diameter, circumference and area
+/*
+int main()
+{
+    float rad, dia, circum, area;
+
+    cout << "Radius: ";
+    cin >> rad;
+
+    dia = 2 * rad;
+    circum = 2 * 3.14 * rad;
+    area = 3.14 * rad *rad;
+
+    cout << "Diameter: " << dia << endl << "Circumference: " << circum << endl << "area: " << area << endl;
+}
+*/
+
+// Q7--Write a Cpp program to enter length in centimeter and convert it into meter and kilometer
+/*
+int main()
+{
+    float cm, m;
+    double km;
+    cout << "length(cm): ";
+    cin >> cm;
+
+    m = cm / 100;
+    km = cm / 100000;
+
+    cout << "length(m): " << m << endl << "length(km): " << km << endl;
+}
+*/
